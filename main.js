8a5fa67e3403c8ce2fbf88ddfde1c47b208bb038
Łukasz <lukasz.poczta12@gmail.com>
2022-08-30 10:41:58 +0200

    created delete option

--- main.js
+++ main.js
@@ -1,60 +1,87 @@
 const incomeArea = document.querySelector('.income-area');
 const expensesArea = document.querySelector('.expenses-area');
-const addTransaction = document.querySelector('.add-transaction');
+const availableMoney = document.querySelector('.available-money');
 const addTransactionPanel = document.querySelector('.add-transaction-panel');
-const cancelBtn = document.querySelector('.cancel');
-const saveBtn = document.querySelector('.save');
-const deleteBtn = document.querySelector('.delete');
+
 const transactionName = document.querySelector('#name');
 const transactionAmount = document.querySelector('#amount');
-const error = document.querySelector('.error');
 const category = document.querySelector('#category');
-const availableMoney = document.querySelector('.available-money');
 
+const addTransactionBtn = document.querySelector('.add-transaction');
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
+const deleteBtn = document.querySelector('.delete');
+const deleteAllBtn = document.querySelector('.delete-all');
+
+const error = document.querySelector('.error');
+const transactionList = document.querySelector('.transaction-list');
+
+let root =document.documentElement;
 let ID = 0;
-let newName;
-let newAmount;
 let categoryIcon;
-const moneyArr = [0];
 let selectedCategory;
+const moneyArr = [0];
+
+
 let sumAvailableMoney = 0;
 
+let incomeToDelete;
+let expenseToDelete
 //////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////
+const showPanel = () => {
+	addTransactionPanel.style.display = 'flex';
+};
+const closePanel = () => {
+	addTransactionPanel.style.display = 'none';
+	transactionDateClear();
+};
+//////////////////////////////////////////////////////
+
 
 const addNewTransaction = () => {
 	if (
-		transactionName.value !== '' &&
-		transactionAmount.value !== '' &&
-		category !== 'none'
-	) {
-		createNewTransaction();
-		error.textContent = '';
-		transactionDateClear();
-		closePanel();
-	} else {
-		error.textContent = 'Musisz uzupełnić wszystkie pola';
-	}
-};
+		transactionName.value !== '' &&transactionAmount.value !== '' &&category !== 'none'
+		) {		createNewTransaction();
+			error.textContent = '';
+			transactionDateClear();
+			closePanel();
+		} 
+		else {
+			error.textContent = 'Musisz uzupełnić wszystkie pola';
+		}
+	};
+	
+const transactionDateClear = () => {
+		transactionName.value = '';
+		transactionAmount.value = '';
+		category.value = 0;
+	};
+	
+//////////////////////////////////////////////////////
+
 
 const createNewTransaction = () => {
-	newTransactionDate();
+	
 	const newTransaction = document.createElement('div');
 	newTransaction.classList.add('transaction');
 	newTransaction.setAttribute('id', ID);
 	checkIcon(selectedCategory);
 	newTransaction.innerHTML = `
     <p class="transaction-name">
-	${categoryIcon} ${newName} </p>
-	<p class="transaction-amount"> ${newAmount} zł<button class="delete"><i class="fas fa-times"></i></button></p>
+	${categoryIcon} ${transactionName.value} </p>
+	<p class="transaction-amount"> ${transactionAmount.value} zł<button class="delete" onclick = 'transactionToClose(${ID})'><i class="fas fa-times"></i></button></p>
 	</div>`;
-	newAmount > 0
+	transactionAmount.value > 0
 		? incomeArea.appendChild(newTransaction) &&
 		  newTransaction.classList.add('income')
 		: expensesArea.appendChild(newTransaction) &&
 		  newTransaction.classList.add('expense');
-	moneyArr.push(parseFloat(newAmount));
+	moneyArr.push(parseFloat(transactionAmount.value));
 	countMoney(moneyArr);
-	
+
 	ID++;
 };
 
@@ -79,34 +106,37 @@ const checkIcon = (transaction) => {
 	}
 };
 const countMoney = (money) => {
-	const newMoney = money.reduce((a,b)=>a+b);
-		availableMoney.textContent = `${newMoney} zł`
-	
+	const newMoney = money.reduce((a, b) => a + b);
+	availableMoney.textContent = `${newMoney} zł`;
 };
 
-const newTransactionDate = () => {
-	newName = transactionName.value;
-	newAmount = transactionAmount.value;
-};
-const transactionDateClear = () => {
-	newName = transactionName.value = '';
-	newAmount = transactionAmount.value = '';
-	category.value = 'none';
+
+
+
+const transactionToClose = (id)=>{
+let closeTransaction = document.getElementById(id)
+incomeArea.removeChild(closeTransaction)
+}
+
+
+
+
+
+const deleteAllTransaction = () => {
+	incomeToDelete = document.getElementsByClassName('income');
+	expenseToDelete = document.getElementsByClassName('expense');
+	while (incomeToDelete.length > 0) incomeToDelete[0].remove();
+	while (expenseToDelete.length > 0) expenseToDelete[0].remove();
+	moneyArr.length = 0;
+	availableMoney.textContent=""
 };
 
 //////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////
-const showPanel = () => {
-	addTransactionPanel.style.display = 'flex';
-};
-const closePanel = () => {
-	addTransactionPanel.style.display = 'none';
-	transactionDateClear();
-};
-//////////////////////////////////////////////////////
 
-addTransaction.addEventListener('click', showPanel);
+
+addTransactionBtn.addEventListener('click', showPanel);
 cancelBtn.addEventListener('click', closePanel);
 
 saveBtn.addEventListener('click', addNewTransaction);
+deleteAllBtn.addEventListener('click', deleteAllTransaction);
